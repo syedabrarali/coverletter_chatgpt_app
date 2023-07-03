4d62fd74d1b73ecf2933aeb81a6fe1367abcc7d4
Abrar <abrar.ali75@gmail.com>
2023-07-03 16:52:22 +0530

    added html svg and css files, created navbar using bootstrap

--- coverletter_app/settings.py
+++ coverletter_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cv_chatgpt.apps.CvChatgptConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
